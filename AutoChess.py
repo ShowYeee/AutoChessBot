f3c3152e81785122a4dc290d2940dfa1af0f75f6
ShowYeee <39395238+ShowYeee@users.noreply.github.com>
2019-03-15 06:59:08 +0800

    Update AutoChess.py

--- AutoChess.py
+++ AutoChess.py
@@ -74,14 +74,14 @@ class Info:
         '♖ Fortress1','♖ Fortress2','♖ Fortress3','♖ Fortress4','♖ Fortress5','♖ Fortress6','♖ Fortress7','♖ Fortress8','♖ Fortress9',
         '♕ King','♔ Queen'])
         plt.xticks(xx,time)
-        plt.plot(xx,yy, linewidth=2, color='black', marker='o', markerfacecolor='red', markersize=4)
+        plt.plot(xx,yy, linewidth=2, color='red', marker='o', markerfacecolor='black', markersize=6)
         #plt.plot(xx,yy, '-',xx, yy,'o')
         plt.xlim([xx.min(),xx.max()])  # x軸邊界
         plt.ylim([yy.min()-1,yy.max()+1])  # y軸邊界
         plt.yticks(fontsize=10)
         plt.xticks(fontsize=8)
         plt.grid(linestyle='-.') 
-        plt.fill_between(xx, yy, interpolate=True, color='green', alpha=0.3)
+        plt.fill_between(xx, yy, interpolate=True, color='gray', alpha=0.2)
         plt.savefig(steamID + '.png', bbox_inches='tight')
         
     
