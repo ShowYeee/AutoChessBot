1ab083e95d1af49378a006c9a147734000b23bf3
ShowYeee <39395238+ShowYeee@users.noreply.github.com>
2019-03-15 07:24:59 +0800

    Update AutoChess.py

--- AutoChess.py
+++ AutoChess.py
@@ -134,7 +134,7 @@ async def rank(ctx , steamID=None):
         embed.add_field(name= '⁕ 遊玩場次',  value = x +  str(theinfo.matches), inline=False)
         embed.add_field(name= '⁕ 糖果數量',  value = x +  str(theinfo.candy), inline=False)
         embed.add_field(name= '⁕ 信使數量',  value = x +  str(theinfo.couriers), inline=False)
-        embed.add_field(name= '⁕ 走勢圖',  value = '`Rating`', inline=False)
+        embed.add_field(name= '⁕ 走勢圖',  value = 'Beta', inline=False)
         embed.set_image(url= uploaded_image.link)
         await ctx.send(embed=embed)
         print("(",strftime("%Y-%m-%d %H:%M:%S", gmtime()),"):",ctx.author.name,"(",ctx.author.id,"),Success(",steamID,")",uploaded_image.link)  
