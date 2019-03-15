912f95e5f62aa6826049e41dd0de6b244a0d63a0
ShowYeee <39395238+ShowYeee@users.noreply.github.com>
2019-03-15 08:27:04 +0800

    Update AutoChess.py

--- AutoChess.py
+++ AutoChess.py
@@ -66,7 +66,7 @@ class Info:
         y.reverse()
         xx = np.array(x)
         yy = np.array(y)
-        plt.figure(figsize=(9,4)
+        plt.figure(figsize=(9,4))
         plt.subplot(axisbg='#444444')
         plt.yticks([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38],
         ['Noob','♙ Soldier1','♙ Soldier2','♙ Soldier3','♙ Soldier4','♙ Soldier5','♙ Soldier6','♙ Soldier7','♙ Soldier8','♙ Soldier9',
