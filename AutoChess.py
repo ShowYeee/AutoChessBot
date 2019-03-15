e5697e1a8374e72e6d171cd61a3dc43449fd50fe
ShowYeee <39395238+ShowYeee@users.noreply.github.com>
2019-03-15 08:50:31 +0800

    Update AutoChess.py

--- AutoChess.py
+++ AutoChess.py
@@ -67,8 +67,9 @@ class Info:
         y.reverse()
         xx = np.array(x)
         yy = np.array(y)
-        plt.rcParams['figure.facecolor'] = 'black'
         plt.figure(figsize=(9,4))
+        plt.rcParams['figure.facecolor'] = 'black'
+        plt.rcParams['axes.facecolor'] = 'black'
         plt.yticks([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38],
         ['Noob','♙ Soldier1','♙ Soldier2','♙ Soldier3','♙ Soldier4','♙ Soldier5','♙ Soldier6','♙ Soldier7','♙ Soldier8','♙ Soldier9',
         '♘ Knight1','♘ Knight2','♘ Knight3','♘ Knight4','♘ Knight5','♘ Knight6','♘ Knight7','♘ Knight8','♘ Knight9',
